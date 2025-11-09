ff61d4a5bfd1f896693f35baba454e3eaf226f27
EmberAxes <olivia.s.reiff@outlook.com>
2025-11-08 19:40:45 -0600

    Learning entry

--- practice.py
+++ practice.py
@@ -1,7 +1,29 @@
 import tkinter as tk
 
 window = tk.Tk()
-greeting = tk.Label(text="Hello World")
-greeting.pack()
+
+### Labels
+label = tk.Label(
+    text="This is a label",
+    fg="purple",  # Set the text color to whatever, foreground
+    bg="white",  # Set the background color to whatever, background
+    width = 10,
+    height = 10
+)
+label.pack()      # Need this to put the label together
+
+### Buttons
+button = tk.Button(text="Click!", width=25, height=5)
+button.pack()
+
+### Entry aka get user input
+entry = tk.Entry(
+    fg="orange", 
+    bg="black"
+)
+entry.insert(0, "widget")    # at index 0 put widget
+entry.delete(4)              # delete the character at index 4
+entry.pack()
+
 
 window.mainloop() # This is what actully makes the window
